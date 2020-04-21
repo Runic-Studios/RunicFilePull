b8d783230b404ec4bd1fb15ac5324a691af0efa9
excel619 <kai.lo@icloud.com>
2020-04-21 19:40:21 +0300

    Fixed issues

--- src/main/java/com/runicrealms/runicfilepull/Plugin.java
+++ src/main/java/com/runicrealms/runicfilepull/Plugin.java
@@ -5,21 +5,28 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.kohsuke.github.GHContent;
-import org.kohsuke.github.GHRepository;
-import org.kohsuke.github.GitHub;
-import org.kohsuke.github.GitHubBuilder;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.yaml.snakeyaml.external.biz.base64Coder.Base64Coder;
 
 import java.io.*;
+import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLConnection;
-import java.util.List;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+import java.util.logging.Level;
 
 public class Plugin extends JavaPlugin implements CommandExecutor {
 
 	private static Plugin plugin;
 	private static boolean isRunning = false;
+
+	private static volatile Integer totalFiles = 0;
+	private static volatile Integer filesCompleted = 0;
 	
 	@Override
 	public void onEnable() {
@@ -38,73 +45,88 @@ public class Plugin extends JavaPlugin implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		if (!isRunning) {
 			isRunning = true;
-			Bukkit.getScheduler().runTaskAsynchronously(this, new Runnable() {
+
+			Bukkit.broadcastMessage(ChatColor.GREEN + "File Pull Initiated - Server Will Restart On Completion");
+			Bukkit.broadcastMessage(ChatColor.DARK_GREEN + "WARNING - Mobs and quests may break while File Pull is running!");
+
+			Bukkit.getScheduler().runTaskAsynchronously(this, () -> {try {mirrorFiles("mobs", "MythicMobs/Mobs");} catch (Exception exception) {Bukkit.broadcastMessage(ChatColor.RED + "There was an issue downloading the mobs! Check the console for more information. Some files may be be missing."); exception.printStackTrace();}});
+			Bukkit.getScheduler().runTaskAsynchronously(this, () -> {try {mirrorFiles("skills", "MythicMobs/Skills");} catch (Exception exception) {Bukkit.broadcastMessage(ChatColor.RED + "There was an issue downloading the skills! Check the console for more information. Some files may be be missing."); exception.printStackTrace();}});
+			Bukkit.getScheduler().runTaskAsynchronously(this, () -> {try {mirrorFiles("spawners", "MythicMobs/Spawners");} catch (Exception exception) {Bukkit.broadcastMessage(ChatColor.RED + "There was an issue downloading spawners! Check the console for more information. Some files may be be missing."); exception.printStackTrace();}});
+			Bukkit.getScheduler().runTaskAsynchronously(this, () -> {try {mirrorFiles("items", "MythicMobs/Items");} catch (Exception exception) {Bukkit.broadcastMessage(ChatColor.RED + "There was an issue downloading the items! Check the console for more information. Some files may be be missing."); exception.printStackTrace();}});
+			Bukkit.getScheduler().runTaskAsynchronously(this, () -> {try {mirrorFiles("quests", "RunicQuests/quests");} catch (Exception exception) {Bukkit.broadcastMessage(ChatColor.RED + "There was an issue downloading the quests! Check the console for more information. Some files may be be missing."); exception.printStackTrace();}});
+
+			new BukkitRunnable() {
 				@Override
 				public void run() {
-					try {
-						if (!getDataFolder().exists()) {
-							getDataFolder().mkdir();
+					if (totalFiles != 0) {
+						if (filesCompleted >= totalFiles) {
+							Bukkit.broadcastMessage(ChatColor.GREEN + "Done! " + ChatColor.DARK_RED + "Server restarting in " + ChatColor.RED + "5" + ChatColor.DARK_RED + " seconds");
+							Bukkit.getScheduler().runTaskLater(Plugin.this, () -> Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "rstop"), 5 * 20);
+							this.cancel();
+							return;
 						}
-						Bukkit.broadcastMessage(ChatColor.GREEN + "File Pull Initiated - Server Will Restart On Completion");
-						GitHub github = new GitHubBuilder().withOAuthToken("8b7fd07ca5e9d08bfc8b878ab0da2913c2a62014").build();
-						GHRepository repo = github.getRepository("Skyfallin/writer-files");
-						Bukkit.broadcastMessage(ChatColor.GREEN + "Downloading mobs " + ChatColor.GRAY + "[0/5]");
-						mirrorFiles(repo, "mobs", "MythicMobs/Mobs");
-						Bukkit.broadcastMessage(ChatColor.GREEN + "Downloading skills " + ChatColor.GRAY + "[1/5]");
-						mirrorFiles(repo, "skills", "MythicMobs/Skills");
-						Bukkit.broadcastMessage(ChatColor.GREEN + "Downloading spawners " + ChatColor.GRAY + "[2/5]");
-						mirrorFiles(repo, "spawners", "MythicMobs/Spawners");
-						Bukkit.broadcastMessage(ChatColor.GREEN + "Downloading items " + ChatColor.GRAY + "[3/5]");
-						mirrorFiles(repo, "items", "MythicMobs/Items");
-						Bukkit.broadcastMessage(ChatColor.GREEN + "Downloading quests " + ChatColor.GRAY + "[4/5]");
-						mirrorFiles(repo, "quests", "RunicQuests/quests");
-						Bukkit.broadcastMessage(ChatColor.GREEN + "Done! " + ChatColor.GRAY + "[5/5]" + ChatColor.DARK_RED + " Server restarting in " + ChatColor.RED + "5" + ChatColor.DARK_RED + " seconds");
-						Bukkit.getScheduler().runTaskLater(Plugin.this, () -> Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "rstop"), 5 * 20);
-					} catch (Exception exception) {
-						Bukkit.broadcastMessage("&aThere was an issue downloading the files from gitlab! Check the console for more information. Some files may be be missing.");
-						exception.printStackTrace();
+						double percentage = Math.round(((100.0 * filesCompleted) / totalFiles * 1.0) * 10.0) / 10.0;
+						for (Player player : Bukkit.getOnlinePlayers()) {
+							player.sendTitle("", ChatColor.GREEN + "File Pull: " + percentage + "%", 0, 40, 0);
+						}
+						Bukkit.getLogger().log(Level.INFO, "[RunicFilePull] File Pull Progress: " + percentage + "%");
+					} else {
+						for (Player player : Bukkit.getOnlinePlayers()) {
+							player.sendTitle("", ChatColor.GREEN + "File Pull: Loading...", 0, 40, 0);
+						}
+						Bukkit.getLogger().log(Level.INFO, "[RunicFilePull] File Pull Progress: Loading...");
 					}
 				}
-			});
+			}.runTaskTimerAsynchronously(this, 20L, 20L);
+
 		} else {
 			sender.sendMessage(ChatColor.RED + "FilePull is already running!");
 		}
 		return true;
 	}
 
-	public static void writeDataFromUrl(String url, File file) throws Exception {
-		URL website = new URL(url);
-		URLConnection connection = website.openConnection();
-		BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-		String inputLine;
-		BufferedWriter writer = new BufferedWriter(new FileWriter(file));
-		while ((inputLine = reader.readLine()) != null) {
-			writer.write(inputLine);
-			writer.newLine();
+	public static void writeBase64ToFile(String base64, File file) throws Exception {
+	    base64 = base64.replaceAll("\\n", "");
+	    while (base64.length() % 4 > 0) {
+	    	base64 += '=';
 		}
-		reader.close();
-		writer.close();
+	    PrintWriter writer = new PrintWriter(file);
+		writer.print(Base64Coder.decodeString(base64));
+        writer.close();
 	}
 
-	public static void mirrorFiles(GHRepository repo, String ghPath, String localPath) throws Exception {
-		List<GHContent> mobsFolder = repo.getDirectoryContent(ghPath);
+	public static void mirrorFiles(String ghPath, String localPath) throws Exception {
+		JSONArray files = (JSONArray) (new JSONParser()).parse(getWithAuth("https://api.github.com/repos/Skyfallin/writer-files/contents/" + ghPath, "runicrealmsgithub:runicrealmsPASSWORD"));
+		totalFiles += files.size();
 		File destination = new File(Plugin.getInstance().getDataFolder().getParent(), localPath);
 		if (!destination.exists()) {
 			destination.mkdirs();
 		}
 		for (File file : destination.listFiles()) {
-			if (file.isFile()) {
-				file.delete();
-			}
-		}
-		for (GHContent content : mobsFolder) {
-			File file = new File(destination, content.getName());
 			if (file.exists()) {
 				file.delete();
 			}
-			file.createNewFile();
-			writeDataFromUrl(content.getDownloadUrl(), file);
+		}
+		for (Object object : files.toArray()) {
+			File localFile = new File(destination, (String) ((JSONObject) object).get("name"));
+			JSONObject gitJson = (JSONObject) (new JSONParser()).parse(getWithAuth((String) ((JSONObject) object).get("url"), "runicrealmsgithub:runicrealmsPASSWORD"));
+			writeBase64ToFile((String) gitJson.get("content"), localFile);
+			filesCompleted++;
 		}
 	}
+
+	public static String getWithAuth(String url, String auth) throws Exception {
+        HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+        connection.setRequestMethod("GET");
+        connection.setDoOutput(true);
+        connection.setRequestProperty("Authorization", "Basic " + Base64.getEncoder().encodeToString(auth.getBytes(StandardCharsets.UTF_8)));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String output = "";
+        String line;
+        while ((line = reader.readLine()) != null) {
+            output += line + "\n";
+        }
+        return output;
+    }
 
 }
