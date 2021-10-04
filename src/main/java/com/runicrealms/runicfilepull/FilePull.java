9b13880f6d447de5c6186c7b690e4d4a3d0629bf
Excel619 <excel8392@gmail.com>
2021-10-04 17:36:26 -0600

    Fixed FP restarting on skills when it shouldn't

--- src/main/java/com/runicrealms/runicfilepull/FilePull.java
+++ src/main/java/com/runicrealms/runicfilepull/FilePull.java
@@ -75,7 +75,7 @@ public class FilePull {
                 public void run() {
                     if (downloadsStarted.get() == downloadsNeeded.get()) {
                         if (filesCompleted.get() >= totalFiles.get()) {
-                            if (folders.get(FilePullFolder.MYTHIC_ITEMS) || folders.get(FilePullFolder.MOBS)) {
+                            if (folders.get(FilePullFolder.MYTHIC_ITEMS) || folders.get(FilePullFolder.MOBS) || folders.get(FilePullFolder.SKILLS)) {
                                 Bukkit.getScheduler().runTaskLater(Plugin.getInstance(), () -> {reset(); Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "mm reload");}, 5 * 20);
                                 Bukkit.getOnlinePlayers().forEach(player -> player.sendTitle("", ChatColor.GREEN + "Done! " + ChatColor.DARK_RED + "Reloading MythicMobs in " + ChatColor.RED + "5" + ChatColor.DARK_RED + "...", 0, 40, 0));
                                 Bukkit.getScheduler().runTaskLater(Plugin.getInstance(), () -> Bukkit.getOnlinePlayers().forEach(player -> player.sendTitle("", ChatColor.DARK_RED + "Reloading MythicMobs in " + ChatColor.RED + "4" + ChatColor.DARK_RED + "...", 0, 40, 0)), 20L);
