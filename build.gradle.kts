8111570df87bdf4216446cf8d1c200cf13aa80bc
Excel619 <excel8392@gmail.com>
2023-05-20 23:48:52 -0600

    Updated buildscript

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 
 group = "com.runicrealms.plugin"
 version = "1.0-SNAPSHOT"
+val artifactName = "filepull"
 
 repositories {
     maven("https://mvnrepository.com/artifact/org.apache.httpcomponents/httpclient")
@@ -27,12 +28,9 @@ publishing {
     publications {
         create<MavenPublication>("maven") {
             groupId = "com.runicrealms.plugin"
-            artifactId = "filepull"
+            artifactId = artifactName
             version = "1.0-SNAPSHOT"
             from(components["java"])
         }
     }
 }
-
-tasks.register("wrapper")
-tasks.register("prepareKotlinBuildScriptModel")
