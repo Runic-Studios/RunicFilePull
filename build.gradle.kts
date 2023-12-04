8283c0cfd066efa508f8ece096eba2bfe2e1dee0
Excel619 <excel8392@gmail.com>
2023-12-04 12:59:13 -0500

    Updated build.gradle.kts to improved superproject version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,3 @@
-val artifactName = "filepull"
-val rrGroup: String by rootProject.extra
-val rrVersion: String by rootProject.extra
-
-plugins {
-    `java-library`
-    `maven-publish`
-}
-
-group = rrGroup
-version = rrVersion
-
-repositories {
-    maven("https://mvnrepository.com/artifact/org.apache.httpcomponents/httpclient")
-    maven("https://mvnrepository.com/artifact/commons-io/commons-io")
-}
-
 dependencies {
     compileOnly(commonLibs.paper)
     compileOnly(commonLibs.spigot)
@@ -24,19 +7,4 @@ dependencies {
     compileOnly(commonLibs.acf)
     compileOnly(project(":Projects:Common"))
     compileOnly(commonLibs.jgit)
-}
-
-java {
-    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            groupId = rrGroup
-            artifactId = artifactName
-            version = rrVersion
-            from(components["java"])
-        }
-    }
 }
